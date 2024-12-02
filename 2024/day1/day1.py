dc3245ced3374950bb7116528f6919483a7e74e2
AfraIsNotAvailable <afrasinei.serban@gmail.com>
2024-12-02 11:33:33 +0200

    first day done

---
+++ 2024/day1/day1.py
@@ -0,0 +1,28 @@
+def part1(file_txt):
+    file_txt = open(file_txt, 'r')
+    diff_sum = 0
+    first = []
+    second = []
+    for line in file_txt:
+        numbers = line.split()
+        first.append(int(numbers[0]))
+        second.append(int(numbers[1]))
+
+    first.sort()
+    second.sort()
+
+    for i in range(len(first)):
+        diff_sum += abs(first[i] - second[i])
+    return first, second
+
+def part2(file_txt):
+    file = open(file_txt, 'r')
+    first, second = part1(file_txt)
+    similarity = 0;
+    for e in first:
+        similarity += e * second.count(e)
+
+    print(similarity)
+
+if __name__ == '__main__':
+    part2("input")
